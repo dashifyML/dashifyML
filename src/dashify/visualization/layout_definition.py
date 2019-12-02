116818224dc7f38d6c98cde1c0ef8b735262e07b
Max Luebbering <m.luebbering@gmail.com>
2019-12-02 21:18:12 +0100

    App is now in a separate file to make easier to import for all the callbacks

--- src/dashify/visualization/layout_definition.py
+++ src/dashify/visualization/layout_definition.py
@@ -1,14 +1,15 @@
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+from dashify.visualization.tabs.tab_graph_grids import render_graphs
 from dashify.visualization import Settings
+from dashify.visualization.tabs.tab_gridsearch_table import render_table
+from dashify.visualization.tabs.tab_settings import render_settings
+from dashify.visualization.app import app
+import uuid
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.config.suppress_callback_exceptions = True
-
-app.layout = html.Div(children=[
-    html.H1(children='Dashify'),
+layout = html.Div(children=[
+    html.H1(children='D-a-s-h-i-f-y'),
 
     html.Div(children=f"analyzing {Settings.log_dir}", id="log-dir"),
 
@@ -18,5 +19,31 @@ app.layout = html.Div(children=[
         dcc.Tab(label='Graphs', value='tab-graphs'),
     ]),
     html.Div(id='tabs-content'),
+    # super ugly for session ids... but Dash wants it that way.
+    # https://dash.plot.ly/sharing-data-between-callbacks
+    html.Div(str(uuid.uuid4()), id='session-id', style={'display': 'none'}),
+    # yet another ugly hack since callbacks always need an output defined...
+    html.Div(id="hidden-div-placeholder", style={"display":"none"})
 ])
+
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value'), Input("session-id", "children")])
+def render_content(tab, session_id):
+    if tab == 'tab-settings':
+        return html.Div([
+            render_settings(session_id)
+        ])
+    elif tab == 'tab-table':
+        return html.Div([
+            render_table(session_id)
+        ])
+    elif tab == 'tab-graphs':
+        return html.Div(children=render_graphs(Settings.log_dir), id="graph-grids")
+
+# TABLE TAB STUFF
+
+
+
+
 
