edfaf05777259939e8ba5f3197e42938f2876077
Max Luebbering <m.luebbering@gmail.com>
2019-12-08 22:46:03 +0100

    Added type information to experiment.py

--- src/dashify/visualization/data_model/experiment.py
+++ src/dashify/visualization/data_model/experiment.py
@@ -1,29 +1,32 @@
+from typing import Dict
+
+
 class Experiment:
-    def __init__(self, config=None, metrics=None, identifier=None):
+    def __init__(self, config=None, metrics: Dict[str, float]=None, identifier: str=None):
         self._config = config
         self._metrics = metrics
         self._identifier = identifier
 
     @property
-    def config(self):
+    def config(self) -> Dict:
         return self._config
 
     @config.setter
-    def config(self, value):
+    def config(self, value: Dict):
         self._config = value
 
     @property
-    def metrics(self):
+    def metrics(self) -> Dict[str, float]:
         return self._metrics
 
     @metrics.setter
-    def metrics(self, value):
+    def metrics(self, value: Dict[str, float]):
         self._metrics = value
 
     @property
-    def identifier(self):
+    def identifier(self) -> str:
         return self._identifier
 
     @identifier.setter
-    def identifier(self, value):
+    def identifier(self, value: str):
         self._identifier = value
