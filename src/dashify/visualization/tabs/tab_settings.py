06568c87dce707618b1f4745f9cfa35128b7af89
Max Luebbering <m.luebbering@gmail.com>
2019-12-09 16:14:31 +0100

    tab_settings now uses metrics and config controller

--- src/dashify/visualization/tabs/tab_settings.py
+++ src/dashify/visualization/tabs/tab_settings.py
@@ -1,6 +1,6 @@
 import dash_html_components as html
 import dash_core_components as dcc
-from dashify.visualization.cache_controller import cache_controller
+from dashify.visualization.data_controllers import MetricsController, ConfigController
 import dash_table
 from dash.dependencies import Input, Output
 from dashify.visualization.app import app
@@ -16,8 +16,8 @@ def render_settings(session_id: str):
 
 
 def create_configs_settings(session_id: str):
-    options = [{'label': key, 'value': key} for key in cache_controller.get_configs_settings(session_id)]
-    selected_elements = cache_controller.get_selected_configs_settings(session_id)
+    options = [{'label': key, 'value': key} for key in ConfigController.get_configs_settings(session_id)]
+    selected_elements = ConfigController.get_selected_configs_settings(session_id)
     settings = html.Div(
         children=[html.H5("Configs"),
                   dcc.Checklist(
@@ -32,61 +32,62 @@ def create_configs_settings(session_id: str):
 
 def create_metrics_settings_table(session_id: str):
     agg_fun_list = ["min", "mean", "max"]
-    df_metrics_settings = cache_controller.get_metrics_settings(session_id)
+    df_metrics_settings = MetricsController.get_metrics_settings(session_id)
 
     table = html.Div([
-            html.H5("Metrics"),
-            dash_table.DataTable(
-                id='table-metrics',
-                data=df_metrics_settings.to_dict('records'),
-                columns=[
-                    {'id': 'metrics', 'name': 'metrics'},
-                    {'id': 'Selected', 'name': 'Selected', 'presentation': 'dropdown'},
-                    {'id': 'Aggregation', 'name': 'Aggregation', 'presentation': 'dropdown'},
-                    {'id': 'Std_band', 'name': 'Std_band', 'presentation': 'dropdown'},
-                    {'id': 'Grouping parameter', 'name': 'Grouping parameter', 'presentation': 'dropdown'},
-                ],
-                editable=True,
-                dropdown={
-                    'Selected': {
-                        'options': [
-                            {'label': i, 'value': i}
-                            for i in ["y", "n"]
-                        ]
-                    },
-                    'Aggregation': {
-                        'options': [
-                            {'label': i, 'value': i}
-                            for i in agg_fun_list
-                        ]
-                    },
-                    'Std_band': {
-                        'options': [
-                            {'label': i, 'value': i}
-                            for i in ["y", "n"]
-                        ]
-                    },
-                    'Grouping parameter': {
-                        'options': [
-                            {'label': i, 'value': i}
-                            for i in cache_controller.get_selected_configs_settings(session_id)
-                        ]
-                    }
+        html.H5("Metrics"),
+        dash_table.DataTable(
+            id='table-metrics',
+            data=df_metrics_settings.to_dict('records'),
+            columns=[
+                {'id': 'metrics', 'name': 'metrics'},
+                {'id': 'Selected', 'name': 'Selected', 'presentation': 'dropdown'},
+                {'id': 'Aggregation', 'name': 'Aggregation', 'presentation': 'dropdown'},
+                {'id': 'Std_band', 'name': 'Std_band', 'presentation': 'dropdown'},
+                {'id': 'Grouping parameter', 'name': 'Grouping parameter', 'presentation': 'dropdown'},
+            ],
+            editable=True,
+            dropdown={
+                'Selected': {
+                    'options': [
+                        {'label': i, 'value': i}
+                        for i in ["y", "n"]
+                    ]
+                },
+                'Aggregation': {
+                    'options': [
+                        {'label': i, 'value': i}
+                        for i in agg_fun_list
+                    ]
+                },
+                'Std_band': {
+                    'options': [
+                        {'label': i, 'value': i}
+                        for i in ["y", "n"]
+                    ]
+                },
+                'Grouping parameter': {
+                    'options': [
+                        {'label': i, 'value': i}
+                        for i in ConfigController.get_selected_configs_settings(session_id)
+                    ]
                 }
-            ),
-            html.Div(id='table-dropdown-container')
-        ], className="five columns"
+            }
+        ),
+        html.Div(id='table-dropdown-container')
+    ], className="five columns"
     )
     return table
 
 
 @app.callback(
     Output('hidden-div-placeholder', "children"),
-    [Input('Configs', "value"), Input("session-id", "children"), Input('table-metrics', 'data'), Input('table-metrics', 'columns')])
+    [Input('Configs', "value"), Input("session-id", "children"), Input('table-metrics', 'data'),
+     Input('table-metrics', 'columns')])
 def settings_callback(selected_configs, session_id, metric_rows, metric_colums):
     # store config
-    cache_controller.set_selected_configs_settings(session_id, selected_configs)
+    ConfigController.set_selected_configs_settings(session_id, selected_configs)
     # store metrics
     df = pd.DataFrame(metric_rows, columns=[c['name'] for c in metric_colums])
-    cache_controller.set_metrics_settings(session_id, df)
+    MetricsController.set_metrics_settings(session_id, df)
     return html.Div("")
