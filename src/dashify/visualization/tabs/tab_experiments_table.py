6d801259902384401dc33398b0ae670524e1a513
Max Luebbering <m.luebbering@gmail.com>
2019-12-09 16:15:02 +0100

    tab_experiments_table uses now the respective controllers

---
+++ src/dashify/visualization/tabs/tab_experiments_table.py
@@ -0,0 +1,26 @@
+from dash.dependencies import Input, Output
+import dash_table
+from dashify.visualization.app import app
+from dashify.visualization.data_controllers import ExperimentController
+
+
+def render_table(session_id: str):
+    df_experiments = ExperimentController.get_experiments_df(session_id)
+    filters = ExperimentController.get_experiment_filters_string(session_id)
+    return dash_table.DataTable(
+        id='table-filtering-be',
+        columns=[
+            {"name": i, "id": i} for i in sorted(df_experiments.columns)
+        ],
+        filter_action='custom',
+        filter_query=' && '.join(filters)
+    )
+
+
+@app.callback(
+    Output('table-filtering-be', "data"),
+    [Input('table-filtering-be', "filter_query"), Input("hidden-log-dir", "children"), Input("session-id", "children")])
+def update_table(filters, log_dir, session_id):
+    ExperimentController.set_experiment_filters(session_id, filters.split(" && "))
+    df_experiments = ExperimentController.get_experiments_df(session_id)
+    return df_experiments.to_dict('records')
