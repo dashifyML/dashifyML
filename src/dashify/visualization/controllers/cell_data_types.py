ad6951b1c0f80b6ddf0f8eecd74aad2f43cded53
Max Luebbering <m.luebbering@gmail.com>
2020-01-07 20:16:30 +0100

    fixed bug

--- src/dashify/visualization/controllers/cell_data_types.py
+++ src/dashify/visualization/controllers/cell_data_types.py
@@ -3,6 +3,7 @@ import pandas as pd
 from typing import Dict
 import json
 
+
 class SupportedDataTypes(Enum):
     number_type = 1
     string_type = 2
@@ -10,25 +11,38 @@ class SupportedDataTypes(Enum):
     bool_type = 4
 
 
+def is_bool(value) -> bool:
+    return str(value).lower() in ["true", "false"]
+
+
+def is_number(value) -> bool:
+    try:
+        float(str(value))
+        return True
+    except ValueError:
+        return False
+
+
+def is_list(value) -> bool:
+    try:
+        json.loads(str(value))
+        return True
+    except Exception:
+        return False
+
+
+def get_datatype(value):
+    if is_number(value):
+        return SupportedDataTypes.number_type
+    elif is_bool(value):
+        return SupportedDataTypes.bool_type
+    if is_list(value):
+        return SupportedDataTypes.list_type
+    else:
+        return SupportedDataTypes.string_type
+
+
 def infer_datatypes_for_columns(df: pd.DataFrame) -> Dict[str, SupportedDataTypes]:
-
-    def is_bool(value) -> bool:
-        return str(value).lower() in ["true", "false"]
-
-    def is_number(value) -> bool:
-        try:
-            float(str(value))
-            return True
-        except ValueError:
-            return False
-
-    def is_list(value) -> bool:
-        try:
-            json.loads(str(value))
-            return True
-        except Exception:
-            return False
-
     def check_series(series: pd.Series, fun):
         return series.apply(fun).all()
 
@@ -46,7 +60,7 @@ def infer_datatypes_for_columns(df: pd.DataFrame) -> Dict[str, SupportedDataType
     return {col: infer_datatye(df[col]) for col in cols}
 
 
-def convert_string_to_supported_data_type(value: str, data_type: SupportedDataTypes):
+def convert_value_to_supported_data_type(value: str, data_type: SupportedDataTypes):
     if data_type == SupportedDataTypes.number_type:
         return float(value)
     elif data_type == SupportedDataTypes.bool_type:
